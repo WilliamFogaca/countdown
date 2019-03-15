232bab0324ec49f24807cae7268557e16244588a
WilliamFogaca <williamps0810@gmail.com>
2019-03-15 01:37:05 -0300

    Implementeção de mensagemde erro para inputs vazios

--- js/modules/components/ErrorMessage.js
+++ js/modules/components/ErrorMessage.js
@@ -1,13 +1,17 @@
-export default function CreateErrorMessage(event) {
+export default function CreateErrorMessage(event, erroType) {
   const containerMsgErro = document.querySelector('.container-msg-erro');
 
-  const div = document.createElement('div');
-  const span = document.createElement('span');
+    const div = document.createElement('div');
+    const span = document.createElement('span');
 
-  div.classList.add('msg-erro');
-  div.setAttribute('data-erro', event);
-  span.innerText = `Já existe um Countdown para ${event}!`;
+    div.classList.add('msg-erro');
+    div.setAttribute('data-erro', event);
+    if(erroType === 1) {
+      span.innerText = `Já existe um Countdown para ${event}!`;
+    } else if(erroType === 2) {
+      span.innerText = `Os campos acima são obrigatórios!`;
+    }
 
-  div.appendChild(span);
-  containerMsgErro.appendChild(div);
+    div.appendChild(span);
+    containerMsgErro.appendChild(div);
 }
